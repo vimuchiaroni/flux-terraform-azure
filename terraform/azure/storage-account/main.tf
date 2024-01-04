39d1f53d45d8e0a83f84b9a026e538a97ada8685
victor.muchiaroni <victor.muchiaroni@brmalls.com.br>
2024-01-04 15:29:30 -0300

    update replication type to GRS

--- terraform/azure/storage-account/main.tf
+++ terraform/azure/storage-account/main.tf
@@ -8,5 +8,5 @@ resource "azurerm_storage_account" "example" {
   resource_group_name      = azurerm_resource_group.example.name
   location                 = azurerm_resource_group.example.location
   account_tier             = "Standard"
-  account_replication_type = "LRS"
+  account_replication_type = "GRS"
 }
